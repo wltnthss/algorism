a21f13c94150fb92c1593b971ee48701edc353d4
wltnthss <60785586+wltnthss@users.noreply.github.com>
2023-08-18 16:12:32 +0900

    직각 이등변 삼각형 * 찍기

---
+++ for_pratice/TriangleB.java
@@ -0,0 +1,25 @@
+package for_pratice;
+
+import java.util.Scanner;
+
+public class TriangleB {
+    public static void main(String[] args) {
+        
+        System.out.println("왼쪽 아래가 직각인 이등변 삼각형 출력");
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        do{
+            System.out.println("몇 단 삼각형입니까? ");
+            n = sc.nextInt();
+        }while(n <= 0);
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
