d028ba33e6219a005857ebd539452cde3ecda82f
wltnthss <60785586+wltnthss@users.noreply.github.com>
2023-09-11 01:32:33 +0900

    선형검색 배열 중복 검사 로직 추가

---
+++ repeat/c21.java
@@ -0,0 +1,59 @@
+package repeat;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class c21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = 0;
+
+        System.out.print("요솟수 : ");
+        int num = sc.nextInt();
+
+        int[] arr = new int[num];
+
+
+        
+        for(int i=0; i<num; i++){
+
+            System.out.print("x[" + i + "] : ");
+            arr[i] = sc.nextInt();
+
+            for (int j = 0; j < i; j++) {
+ 
+                if (arr[i] == arr[j]) {  // 중복검사
+                    System.out.println("중복되는 수 입니다 다시입력하세요.( 중복숫자 " + arr[j] + ")");
+                    i--;
+                }
+            }
+        }
+
+        System.out.print("검색할 값 : ");
+        int key = sc.nextInt();
+
+        int result = seqSearch(num, arr, key);
+
+        if(result == -1){
+            System.out.print("검색할 값이 배열에 존재하지않습니다.");
+        }else{
+            System.out.print(key + "은(는) " + "x[" + result + "]에 있습니다.");
+        }
+        
+    }
+
+    private static int seqSearch(int num, int[] arr, int key) {
+
+        for(int i=0; i<num; i++){
+            if(key == arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
