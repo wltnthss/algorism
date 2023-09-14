4a1fdbd63cc3f3c28f143cc5e24f3f7029eb9923
wltnthss <60785586+wltnthss@users.noreply.github.com>
2023-09-14 18:32:52 +0900

    대소문자바꾸기 다른방법

--- FastCampus/ch01_문자열/대소문자바꾸기v2.java
+++ FastCampus/ch01_문자열/대소문자바꾸기v2.java
@@ -6,31 +6,17 @@ public class 대소문자바꾸기v2 {
     
     public static void main(String[] args) {
 
-        String result = "";
-
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-
-        // for(int i=0; i<input.length(); i++){
-        //     if('A' <= input.charAt(i) && input.charAt(i) <= 'Z'){
-        //         System.out.println(i + " 대문자 ");
-        //     }else{
-        //         System.out.println(i + " 소문자 ");
-        //     }
-        // }
 
         char[] arr = input.toCharArray();
 
         for(int i=0; i<input.length(); i++){
             if('A' <= input.charAt(i) && input.charAt(i) <= 'Z'){
-                System.out.println(arr[i]);
-                result = result + Character.toLowerCase(arr[i]);
+                System.out.print((char)('a' + input.charAt(i) - 'A'));
             }else{
-                System.out.println(arr[i]);
-                result = result + Character.toUpperCase(arr[i]);
+                System.out.print((char)('A' + input.charAt(i) - 'a'));
             }
         }
-
-        System.out.println(result);
     }
 }
