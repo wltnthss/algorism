dc9f003d23bb510e1fccf286253a4366d3e5da90
손지수 <60785586+wltnthss@users.noreply.github.com>
2023-09-19 18:39:30 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long n1 = sc.nextLong();
+		long n2 = sc.nextLong();
+		long n3 = sc.nextLong();
+		
+		System.out.println(n1 + n2 + n3);
+	}
+}
