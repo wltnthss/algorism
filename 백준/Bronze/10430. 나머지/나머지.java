abed601cbfadb2d1fc13aa126670a7e1f4a89a23
손지수 <60785586+wltnthss@users.noreply.github.com>
2023-09-19 17:06:33 +0900

    [Bronze V] Title: 나머지, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+
+		System.out.println((n1 + n2) % n3);
+		System.out.println(((n1 % n3) + (n2 % n3)) % n3);
+		System.out.println((n1 * n2) % n3);
+		System.out.println(((n1 % n3) * (n2 % n3)) % n3);
+	}
+}
