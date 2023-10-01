65a552b24f10f36c0cabe08d294f1756ba09b525
손지수 <60785586+wltnthss@users.noreply.github.com>
2023-10-01 18:20:55 +0900

    [Bronze III] Title: 알람 시계, Time: 276 ms, Memory: 19676 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+	
+		int H = sc.nextInt(); //시간
+		int M = sc.nextInt(); //분
 		
-		int n1 = sc.nextInt();
-		int n2 = sc.nextInt();
-		
-		/*
-		 * 분이 0밑으로 내려갔을 때 분기
-		 */
-		if(n2 - 45 < 0) {
-			if(n1 == 0) {
-				n1 = 24;
+		// 1.1 M < 45 M이 음수
+		if(M < 45) {
+			H --;
+			M = 60-(45-M);
+			// 2.1 H < 0 H가 음수
+			if(H < 0) {
+				H = 23;
 			}
-			n1--;
-			n2 = 60 + n2 - 45;
-			
-		}else {
-			n2 = n2 - 45;
+			System.out.println(H + " " + M);
 		}
-		System.out.println(n1 + " " + n2);
+		// 1.2 M > 45 M이 양수
+		else {
+			System.out.println(H + " " + (M-45));
+		}
+		
 	}
 }
