791db4876db692532723d64cfe89cc92e3e71234
손지수 <60785586+wltnthss@users.noreply.github.com>
2023-10-03 14:50:13 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/* 출력
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+		 */ 
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
