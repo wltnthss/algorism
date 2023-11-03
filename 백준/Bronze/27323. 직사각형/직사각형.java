1dc84a6cc2581982efc34f504df4df69c4557982
손지수 <60785586+wltnthss@users.noreply.github.com>
2023-11-03 14:57:15 +0900

    [Bronze V] Title: 직사각형, Time: 108 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * 정수 N 소인수분해 결과 오름차순 출력
+		 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A*B);
+	}
+}
