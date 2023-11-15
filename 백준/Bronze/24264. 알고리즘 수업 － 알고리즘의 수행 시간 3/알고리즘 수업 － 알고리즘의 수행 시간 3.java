6f7772d67378ee871be897967644c1ff3ad13f72
손지수 <60785586+wltnthss@users.noreply.github.com>
2023-11-15 16:04:01 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 112 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * 1. i ~ n, j ~ n, 이중배열은 O(n2)
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+		
+		System.out.println(n*n);
+		System.out.println(2);
+	}
+}
