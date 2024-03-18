6710bd4f9d9c656208ad29c2d4304b569178fc29
손지수 <60785586+wltnthss@users.noreply.github.com>
2024-03-18 12:25:19 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        
+        if (ineq.equals(">")) {
+                if (eq.equals("=")) {
+                        return n >= m ? 1 : 0;
+                } else if (eq.equals("!")) {
+                        return n > m ? 1 : 0;
+                }
+        } else if (ineq.equals("<")) {
+                if (eq.equals("=")) {
+                        return n <= m ? 1 : 0;
+                } else if (eq.equals("!")) {
+                        return n < m ? 1 : 0;
+                }
+        }
+
+        return 0; // 기본적으로 0을 반환 (조건에 맞지 않는 경우)
+    }
+}
