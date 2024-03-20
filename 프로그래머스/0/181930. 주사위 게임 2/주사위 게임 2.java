6d993588cee1db0233b4a0a1640e9fbbf665f928
손지수 <60785586+wltnthss@users.noreply.github.com>
2024-03-20 19:17:52 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        if(a == b && b == c && a == c){
+            answer += (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2) ) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+        }else if(a == b){
+            if(b != c){
+                answer += (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+            }
+        }else if(a == c){
+            if (b != a){
+                answer += (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+            }
+        }else if(b == c){
+            if(a != b){
+                 answer += (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+            }   
+        }else{
+            answer += (a + b + c);
+        }
+        
+        return answer;
+    }
+}
